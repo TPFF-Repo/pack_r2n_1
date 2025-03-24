aaf58a938a4424ae57f5ec473d1feb9a9cbde34b
maximemockelyn <maximemockelyn8@gmail.com>
2025-03-24 16:06:33 +0100

    First mod.lua & String.lua

---
+++ mod.lua
@@ -0,0 +1,24 @@
+function data()
+    return {
+        info = {
+            minorVersion = 4,
+            severityAdd = "NONE",
+            severityRemove = "WARNING",
+            name = _("pack_r2n_name"),
+            description = _("pack_r2n_desc"),
+            tags = { "europe", "france", "SNCF", "TPFF", "Train",  },
+            authors = {
+                {
+                    name = "NTH-Z6K4",
+                    role = "CREATOR",
+                    text = "3D, Texture, Script",
+                },
+                {
+                    name = "SYLTHERON",
+                    role = "Creator",
+                    text = "3D, Texture, Script",
+                },
+            },
+        },
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,101 @@
+function data()
+    local lb = '\n'
+    local site = '[url=https://discord.transportfeverfrance.fr/][u]Transport Fever France[/u][/url]'
+
+return {
+fr = {
+    ["pack_r2n_name"] = "Pack R2N",
+    ["gare_ouest_etat_desc"] = "[h1]Pack de gares de Type Ouest-Etat [/h1]"..lb..
+    lb..
+    lb..
+    "Vous retrouverez dans ce pack, plusieurs variétés de gares typées de l'ancienne compagnie de l'Ouest-Etat, représentant ainsi la Normandie, Les Pays de la Loire, et la Bretagne."..lb..
+    lb..
+    lb..
+    "[h2][b]Liste des gares disponibles :[/b][/h2]"..lb..
+    "[list]"..lb..
+    "[*][b]Ancenis [/b]"..lb..
+    "[*][b]Briouze [/b]"..lb..
+    "[*][b]Cholet [/b]"..lb..
+    "[*][b]Guingamp (avec et sans marquise) [/b]"..lb..
+    "[*][b]Montauban de Bretagne (avec lettrages en relief ou à plaque) [/b]"..lb..
+    "[*][b]Nort-Sur-Erdre [/b]"..lb..
+    "[*][b]Plouaret Trégor [/b]"..lb..
+    "[*][b]Pontchâteau [/b]"..lb..
+    "[*][b]Rosporden [/b]"..lb..
+    "[*][b]Yffiniac [/b]"..lb..
+    "[/list]"..lb..
+    lb..
+    "A savoir qu'une version générique (sans le nom des gares) est disponible pour chaque batiments, excepté pour Plouaret Trégor."..lb..
+    "Vous les trouverez alors, soit dans les modules de gares, soit en assets."..lb..
+    lb..
+    lb..
+    "[h2][b]Auteurs :[/b][/h2]"..lb..
+    "[list]"..lb..
+    "[*][b]Modèles 3D : [/b] NTH-Z6K4, Syltheron, Yoodel22"..lb..
+    "[*][b]Textures : [/b] NTH-Z6K4, Syltheron, Yoodel22"..lb..
+    "[*][b]Scripts : [/b] NTH-Z6K4, Syltheron"..lb..
+    "[/list]"..lb..
+    lb..
+    lb..
+    "[h2][b]Update logs[/b][/h2]"..lb..
+    "[list]"..lb..
+    "[*]Main 3.0 : stable"..lb..
+    "[/list]"..lb..
+    lb..
+    lb..
+    "[h2][b]Disclaimer :[/b][/h2]"..lb..
+    "La réutilisation / publication des fichiers, que ce soit sous une forme modifiée ou inchangée (dans des repaints ou autres), nécessite l'autorisation expresse de l'auteur.\n" ..lb..
+    "L'upload de ce mod sur d'autres sites n'est pas autorisée.\n"..lb..
+    lb..
+    "[h2][b]La communauté française :[/b][/h2]"..lb..site.."",
+},
+		
+en = {
+    ["pack_r2n_name"] = "Pack R2N",
+    ["gare_ouest_etat_desc"] = "[h1]West-Nation Type Stations Pack[/h1]"..lb..
+    lb..
+    lb..
+    "In this pack, you will find several varieties of stations inspired by the former West-Nation company, representing Normandy, Pays de la Loire, and Brittany."..lb..
+    lb..
+    lb..
+    "[h2][b]List of available stations:[/b][/h2]"..lb..
+    "[list]"..lb..
+    "[*][b]Ancenis [/b]"..lb..
+    "[*][b]Briouze [/b]"..lb..
+    "[*][b]Cholet [/b]"..lb..
+    "[*][b]Guingamp (with and without canopy) [/b]"..lb..
+    "[*][b]Montauban de Bretagne (with embossed lettering or plaque) [/b]"..lb..
+	"[*][b]Nort-Sur-Erdre [/b]"..lb..
+    "[*][b]Plouaret Trégor [/b]"..lb..
+    "[*][b]Pontchâteau [/b]"..lb..
+    "[*][b]Rosporden [/b]"..lb..
+    "[*][b]Yffiniac [/b]"..lb..
+    "[/list]"..lb..
+    lb..
+    "Note that a generic version (without station names) is available for each building, except for Plouaret Trégor."..lb..
+    "You will find them either in the station modules or as assets."..lb..
+    lb..
+    lb..
+    "[h2][b]Authors:[/b][/h2]"..lb..
+    "[list]"..lb..
+    "[*][b]3D Models: [/b] NTH-Z6K4, Syltheron, Yoodel22"..lb..
+    "[*][b]Textures: [/b] NTH-Z6K4, Syltheron, Yoodel22"..lb..
+    "[*][b]Scripts: [/b] NTH-Z6K4, Syltheron"..lb..
+    "[/list]"..lb..
+    lb..
+    lb..
+    "[h2][b]Update logs[/b][/h2]"..lb..
+    "[list]"..lb..
+    "[*]Main 3.0: stable"..lb..
+    "[/list]"..lb..
+    lb..
+    lb..
+    "[h2][b]Disclaimer:[/b][/h2]"..lb..
+    "Reusing/publishing files, whether modified or unmodified (in repaints or otherwise), requires the express permission of the author.\n" ..lb..
+    "Uploading this mod to other sites is not allowed.\n"..lb..
+    lb..
+    "[h2][b]The French Community:[/b][/h2]"..lb..site.."",
+},
+
+}
+end
